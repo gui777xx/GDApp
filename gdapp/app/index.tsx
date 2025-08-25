0fc45ba8344b1ba3b40d4f14fbe34f37ad93250b
gui777x <guizin.clementino@gmail.com>
2025-08-25 10:49:19 -0300

    Criado o projeto e iniciado o CustomHeader

---
+++ gdapp/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginTop: 60,
+        backgroundColor: 'violet'
+        
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
---
+++ gdapp/constants/colors.ts
@@ -0,0 +1,25 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+    light: {
+        text: '#000',
+        background:' #fff',
+        tint: tintColorLight,
+        tabIconDefault:'#cccc',
+        tabIconSelected: tintColorLight
+    },
+    dark: {
+        text: '#fff',
+        background:' #000',
+        tint: tintColorDark,
+        tabIconDefault:'#cccc',
+        tabIconSelected: tintColorDark
+    },
+    primary:'#20E1B2',
+    lightGrey:'#FCF8FF',
+    grey:'#EEE9F0',
+    medium:'#9F9AA1',
+    mediumDark:'#424242',
+    green:'#437919',
+}
